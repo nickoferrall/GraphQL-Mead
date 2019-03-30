15d18ea5d1b214c6a93d81a8bf54436cd2f57179
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-03-30 16:53:57 -0500

    Added subscriptions

--- udemy/graphql-basics/src/resolvers/Mutation.js
+++ udemy/graphql-basics/src/resolvers/Mutation.js
@@ -2,11 +2,10 @@ import uuidv4 from 'uuid/v4';
 
 const Mutation = {
   createUser(parent, args, { db }, info) {
-    const emailTaken = db.users.some(user => {
-      return user.email === args.data.email;
-    });
+    const emailTaken = db.users.some(user => user.email === args.data.email);
+
     if (emailTaken) {
-      throw new Error('Email taken!');
+      throw new Error('Email taken');
     }
 
     const user = {
@@ -19,11 +18,13 @@ const Mutation = {
     return user;
   },
   deleteUser(parent, args, { db }, info) {
-    const userIndex = users.findIndex(user => user.id === args.id);
+    const userIndex = db.users.findIndex(user => user.id === args.id);
+
     if (userIndex === -1) {
       throw new Error('User not found');
     }
-    const deletedUsers = users.splice(userIndex, 1);
+
+    const deletedUsers = db.users.splice(userIndex, 1);
 
     db.posts = db.posts.filter(post => {
       const match = post.author === args.id;
@@ -38,7 +39,6 @@ const Mutation = {
 
     return deletedUsers[0];
   },
-
   updateUser(parent, args, { db }, info) {
     const { id, data } = args;
     const user = db.users.find(user => user.id === id);
@@ -53,11 +53,12 @@ const Mutation = {
       if (emailTaken) {
         throw new Error('Email taken');
       }
+
       user.email = data.email;
     }
 
     if (typeof data.name === 'string') {
-      user.name === data.name;
+      user.name = data.name;
     }
 
     if (typeof data.age !== 'undefined') {
@@ -66,8 +67,7 @@ const Mutation = {
 
     return user;
   },
-
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
 
     if (!userExists) {
@@ -81,27 +81,93 @@ const Mutation = {
 
     db.posts.push(post);
 
+    if (args.data.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
+      });
+    }
+
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
 
     if (postIndex === -1) {
       throw new Error('Post not found');
     }
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
-    db.comments = db.comments.filter(comment => comment.post !== args.post);
+    db.comments = db.comments.filter(comment => comment.post !== args.id);
 
-    return deletedPosts[0];
+    if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post
+        }
+      });
+    }
+
+    return post;
   },
-  createComment(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub }, info) {
+    const { id, data } = args;
+    const post = db.posts.find(post => post.id === id);
+    const originalPost = { ...post };
+
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    if (typeof data.title === 'string') {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === 'string') {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost
+          }
+        });
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post
+          }
+        });
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post
+        }
+      });
+    }
+
+    return post;
+  },
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
-    const postExists = db.posts.some(post => post.id === args.data.post);
+    const postExists = db.posts.some(
+      post => post.id === args.data.post && post.published
+    );
 
     if (!userExists || !postExists) {
-      throw new Error('User and post not found');
+      throw new Error('Unable to find user and post');
     }
 
     const comment = {
@@ -111,9 +177,16 @@ const Mutation = {
 
     db.comments.push(comment);
 
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        mutation: 'CREATED',
+        data: comment
+      }
+    });
+
     return comment;
   },
-  deleteComment(parent, args, { db }, info) {
+  deleteComment(parent, args, { db, pubsub }, info) {
     const commentIndex = db.comments.findIndex(
       comment => comment.id === args.id
     );
@@ -122,9 +195,37 @@ const Mutation = {
       throw new Error('Comment not found');
     }
 
-    const deletedComments = db.comments.splice(commentIndex, 1);
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-    return deletedComments[0];
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        mutation: 'DELETED',
+        data: deletedComment
+      }
+    });
+
+    return deletedComment;
+  },
+  updateComment(parent, args, { db, pubsub }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find(comment => comment.id === id);
+
+    if (!comment) {
+      throw new Error('Comment not found');
+    }
+
+    if (typeof data.text === 'string') {
+      comment.text = data.text;
+    }
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        mutation: 'UPDATED',
+        data: comment
+      }
+    });
+
+    return comment;
   }
 };
 
--- udemy/graphql-basics/src/resolvers/Query.js
+++ udemy/graphql-basics/src/resolvers/Query.js
@@ -3,32 +3,43 @@ const Query = {
     if (!args.query) {
       return db.users;
     }
+
     return db.users.filter(user => {
       return user.name.toLowerCase().includes(args.query.toLowerCase());
     });
   },
-  me() {
-    return {
-      id: '123456',
-      name: 'John',
-      email: 'bigjohn@gmail.com'
-    };
-  },
-  post(parent, args, { db }, info) {
+  posts(parent, args, { db }, info) {
     if (!args.query) {
       return db.posts;
     }
+
     return db.posts.filter(post => {
-      return post.title.toLowerCase().includes(args.query.toLowerCase());
+      const isTitleMatch = post.title
+        .toLowerCase()
+        .includes(args.query.toLowerCase());
+      const isBodyMatch = post.body
+        .toLowerCase()
+        .includes(args.query.toLowerCase());
+      return isTitleMatch || isBodyMatch;
     });
   },
-  comment(parent, args, { db }, info) {
-    if (!args.query) {
-      return db.comments;
-    }
-    return db.comments.filter(comment => {
-      return comment.text.toLowerCase().includes(args.query.toLowerCase());
-    });
+  comments(parent, args, { db }, info) {
+    return db.comments;
+  },
+  me() {
+    return {
+      id: '123098',
+      name: 'Mike',
+      email: 'mike@example.com'
+    };
+  },
+  post() {
+    return {
+      id: '092',
+      title: 'GraphQL 101',
+      body: '',
+      published: false
+    };
   }
 };
 
