1f7781a26c4e13b2dd014cdd0a674141881f842b
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-20 16:38:57 -0500

    Integrating prisma with project

--- udemy/graphql-prisma/src/resolvers/Mutation.js
+++ udemy/graphql-prisma/src/resolvers/Mutation.js
@@ -1,43 +1,51 @@
 import uuidv4 from 'uuid/v4';
 
 const Mutation = {
-  createUser(parent, args, { db }, info) {
-    const emailTaken = db.users.some(user => user.email === args.data.email);
+  async createUser(parent, args, { prisma }, info) {
+    const emailTaken = await prisma.exists.User({ email: args.data.email });
 
     if (emailTaken) {
       throw new Error('Email taken');
     }
 
-    const user = {
-      id: uuidv4(),
-      ...args.data
-    };
-
-    db.users.push(user);
-
-    return user;
+    return await prisma.mutation.createUser({ data: args.data });
   },
-  deleteUser(parent, args, { db }, info) {
-    const userIndex = db.users.findIndex(user => user.id === args.id);
+  async deleteUser(parent, args, { prisma }, info) {
+    const userExists = await prisma.exists.User({ id: args.id });
 
-    if (userIndex === -1) {
+    if (!userExists) {
       throw new Error('User not found');
     }
 
-    const deletedUsers = db.users.splice(userIndex, 1);
+    return prisma.mutation.deleteUser(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
 
-    db.posts = db.posts.filter(post => {
-      const match = post.author === args.id;
+    // const userIndex = db.users.findIndex(user => user.id === args.id);
 
-      if (match) {
-        db.comments = db.comments.filter(comment => comment.post !== post.id);
-      }
+    // if (userIndex === -1) {
+    //   throw new Error('User not found');
+    // }
 
-      return !match;
-    });
-    db.comments = db.comments.filter(comment => comment.author !== args.id);
+    // const deletedUsers = db.users.splice(userIndex, 1);
 
-    return deletedUsers[0];
+    // db.posts = db.posts.filter(post => {
+    //   const match = post.author === args.id;
+
+    //   if (match) {
+    //     db.comments = db.comments.filter(comment => comment.post !== post.id);
+    //   }
+
+    //   return !match;
+    // });
+    // db.comments = db.comments.filter(comment => comment.author !== args.id);
+
+    // return deletedUsers[0];
   },
   updateUser(parent, args, { db }, info) {
     const { id, data } = args;
