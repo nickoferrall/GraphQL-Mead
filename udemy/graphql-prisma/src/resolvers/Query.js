c2882a41983b1f817b29346294f932d0d47f6932
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-22 18:53:52 -0500

    Added myPosts query

--- udemy/graphql-prisma/src/resolvers/Query.js
+++ udemy/graphql-prisma/src/resolvers/Query.js
@@ -19,20 +19,44 @@ const Query = {
 
     return prisma.query.users(opArgs, info);
   },
-  posts(parent, args, { prisma }, info) {
-    const opArgs = {};
+  myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    const opArgs = {
+      where: {
+        author: {
+          id: userId
+        }
+      }
+    };
+    if (args.query) {
+      opArgs.where.OR = [
+        {
+          title_contains: args.query
+        },
+        {
+          body_contains: args.query
+        }
+      ];
+    }
+
+    return prisma.query.posts(opArgs, info);
+  },
+  posts(parent, args, { prisma, request }, info) {
+    const opArgs = {
+      where: {
+        published: false
+      }
+    };
 
     if (args.query) {
-      opArgs.where = {
-        OR: [
-          {
-            title_contains: args.query
-          },
-          {
-            body_contains: args.query
-          }
-        ]
-      };
+      opArgs.where.OR = [
+        {
+          title_contains: args.query
+        },
+        {
+          body_contains: args.query
+        }
+      ];
     }
 
     return prisma.query.posts(opArgs, info);
