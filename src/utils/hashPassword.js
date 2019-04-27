385b54be9c49682b7e7354137b00b52843828610
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-27 13:04:14 -0500

    Added init and updated gitignore

---
+++ src/utils/hashPassword.js
@@ -0,0 +1,10 @@
+import bcrypt from 'bcryptjs';
+
+const hashPassword = password => {
+  if (password.length < 0) {
+    throw new Error('Password must be 8 characters or longer.');
+  }
+  return bcrypt.hash(password, 10);
+};
+
+export { hashPassword as default };
