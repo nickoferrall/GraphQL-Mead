13783be613ed8bf3646749d5c49bc670a2699264
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-28 13:55:06 -0500

    Updated jwt secret

--- src/utils/generate.js
+++ src/utils/generate.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = userId => {
-  return jwt.sign({ userId }, 'thisisasecret', {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: '7 days'
   });
 };
